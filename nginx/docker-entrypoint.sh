ae8edfd39fce360b982e431de4558d2c275c167d
Sebastien Collin <sebastien.collin@sebcworks.fr>
2018-08-18 16:07:28 +0200

    Update entrypoint for docker and compose compatibility
    Add examples of env_file and now use same environment variables name as mariadb image

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 set -e
 
-if [ -z "$SONEREZH_DB_PORT_3306_TCP_ADDR" ]; then
-	echo >&2 'error: missing required SONEREZH_DB_ENV_MYSQL_PASSWORD environment variable'
-	echo >&2 '	Did you forget to --link some-mysql-container:mysql?'
+#
+# - Take host from link name: MUST BE "sonerezh-db"
+# - Take database name from our own environment variable "MYSQL_DATABASE" (default to sonerezh)
+# - Same for database user "MYSQL_USER" (default to sonerezh) 
+# - Same for database password "MYSQL_PASSWORD" (but no default!)
+#
+
+if [ -z "$MYSQL_DATABASE" ]; then
+	echo >&2 'error: missing required MYSQL_DATABASE environment variable'
 	exit 1
 fi
 
-if [ -z "$SONEREZH_DB_ENV_MYSQL_DATABASE" ]; then
-	echo >&2 'error: missing required SONEREZH_DB_ENV_MYSQL_PASSWORD environment variable'
-	echo >&2 '	Did you forget to --link some-mysql-container:mysql?'
-	exit 1
-fi
-
-if [ -z "$SONEREZH_DB_ENV_MYSQL_USER" ]; then
+if [ -z "$MYSQL_USER" ]; then
 	echo >&2 'error: missing required MYSQL_USER environment variable'
-	echo >&2 '	Did you forget to --link some-mysql-container:mysql?'
 	exit 1
 fi
 
-if [ -z "$SONEREZH_DB_ENV_MYSQL_PASSWORD" ]; then
+if [ -z "$MYSQL_PASSWORD" ]; then
 	echo >&2 'error: missing required MYSQL_PASSWORD environment variable'
-	echo >&2 '	Did you forget to --link some-mysql-container:mysql?'
 	exit 1
 fi
 
-sed -i "s/SONEREZH_DB_PORT_3306_TCP_ADDR/${SONEREZH_DB_PORT_3306_TCP_ADDR}/" /usr/share/nginx/sonerezh/app/Config/database.php
-sed -i "s/SONEREZH_DB_ENV_MYSQL_DATABASE/${SONEREZH_DB_ENV_MYSQL_DATABASE}/" /usr/share/nginx/sonerezh/app/Config/database.php
-sed -i "s/SONEREZH_DB_ENV_MYSQL_USER/${SONEREZH_DB_ENV_MYSQL_USER}/" /usr/share/nginx/sonerezh/app/Config/database.php
-sed -i "s/SONEREZH_DB_ENV_MYSQL_PASSWORD/${SONEREZH_DB_ENV_MYSQL_PASSWORD}/" /usr/share/nginx/sonerezh/app/Config/database.php
+sed -i "s/SONEREZH_DB_PORT_3306_TCP_ADDR/sonerezh-db/" /usr/share/nginx/sonerezh/app/Config/database.php
+sed -i "s/SONEREZH_DB_ENV_MYSQL_DATABASE/${MYSQL_DATABASE}/" /usr/share/nginx/sonerezh/app/Config/database.php
+sed -i "s/SONEREZH_DB_ENV_MYSQL_USER/${MYSQL_USER}/" /usr/share/nginx/sonerezh/app/Config/database.php
+sed -i "s/SONEREZH_DB_ENV_MYSQL_PASSWORD/${MYSQL_PASSWORD}/" /usr/share/nginx/sonerezh/app/Config/database.php
 sed -i "s/define('DOCKER', false)/define('DOCKER', true)/" /usr/share/nginx/sonerezh/app/Config/bootstrap.php
 
 exec "$@"
